652eb9d47b2df176fe238fe64f5089b4e740bf01
Hyun Seung Jeong <ahribori@gmail.com>
2017-08-10 11:44:01 +0900

    - add standalone server

---
+++ server/standalone.js
@@ -0,0 +1,30 @@
+/* =========================================
+ Load dependencies
+ ============================================*/
+import express from 'express';
+import path from 'path';
+import figlet from 'figlet';
+
+/* =========================================
+ Load Config.js
+ ============================================*/
+const port = process.env.PORT || 8088;
+
+/* =========================================
+ Express Configuration
+ ============================================*/
+const app = express();
+
+app.use('/', express.static(path.resolve(__dirname, '../build')));
+
+app.use('*', function (req, res) {
+    res.sendFile(path.resolve(__dirname, '../build/index.html'));
+});
+
+// open the server
+app.listen(port, () => {
+    figlet('React App', (err, data) => {
+        console.log(data);
+        console.log(`Running on port ${port}...`);
+    });
+});
